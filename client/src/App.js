3a9f07b063d9ca53fab41eb752510c6ae36384c6
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-02-26 16:13:24 +0600

    wallet connector

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import { ethers } from 'ethers';
+import abi from './constract/Coffee.json'
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, setState] = useState({
+        provider: null,
+        signer: null,
+        contract: null,
+    });
+
+    useEffect(() => {
+
+        const connectWallet = async () => {
+
+            const contractAddress = 0x124d56Dd049a78AD9826aB6AD93BF03AC638e43C;
+            const contractABI = abi.abi;
+            try {
+                const { ethereum } = window;
+                if (ethereum) {
+                    const account = await ethereum.request({ method: "eth_requestAccounts" });
+                }
+
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+                setState({
+                    ...state,
+                    provider,
+                    signer,
+                    contract,
+                })
+
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        connectWallet()
+    }, [])
+
+    console.log(state);
+
+
+    return <>
+        <h1>Hello Wrold</h1>
+    </>
+
+
+
 }
 
 export default App;
